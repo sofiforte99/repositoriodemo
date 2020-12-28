231319dd3a59a20491322abff8d5942e9a2a0ec3
sofiforte99 <64369371+sofiforte99@users.noreply.github.com>
2020-12-28 13:14:49 -0300

    commit on 'master'

---
+++ src/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function NavBar() {
+    
+    return(
+        <>
+        <header>
+            <h1>Tienda de Empanadas</h1>
+        </header>
+        <nav>
+            <p>Home</p>
+            <p>Productos</p>
+            <p>Mi Cuenta</p>
+        </nav>
+        </>
+    ) 
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavBar from './NavBar';
 import reportWebVitals from './reportWebVitals';
+
+
+//JSK
+
+const Componente = <p>Hola Mundo</p>
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  <NavBar/>,Componente,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
