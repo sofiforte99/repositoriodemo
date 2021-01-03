9a00110ba3946cb604be032be02cb2610e1bfa68
sofiforte99 <64369371+sofiforte99@users.noreply.github.com>
2021-01-03 18:56:51 -0300

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import React , {useState} from "react";
 import './App.css';
 import './index.js';
 import NavBar from './NavBar.js';
 import ItemListContainer from './ItemListContainer.js';
-
+import ItemCount from './ItemCount.js';
 
 
 function App() {
 
-  const [contador, setContador] = useState (0)
-
-  const aumentarContador = () =>
-  {
-    setContador(contador + 1)
-  }
-
-  const restarContador = () =>
-  {
-    setContador(contador - 1)
-  }
   return (
     <>
    <NavBar/>
    <ItemListContainer greeting ="Buen día"/>
-   <p>El contador va: {contador}</p>
-   <button onClick = {aumentarContador}> + </button>
-   <button onClick = {restarContador}> - </button>
-    </>
+   <ItemCount/>
+   </>
   );
 }
 
---
+++ src/ItemCount.js
@@ -0,0 +1,39 @@
+
+import React, {useState} from "react"; 
+const ItemCount = () =>
+{
+    const [contador, setContador] = useState(1)
+    const [stock] = useState(10)
+   
+    const aumentarContador = () =>
+    {
+        if (contador<=stock)
+        {
+        setContador(contador + 1)
+        }
+    }
+
+    const resetearContador = () =>
+    {
+        setContador(0)
+    }
+
+    const restarContador = () =>
+    {
+        setContador(contador - 1)
+    }
+
+
+    return(
+        <>
+        <p>El contador va: {contador}</p>
+        <button onClick = {aumentarContador}> + </button>
+        <button onClick = {resetearContador}>Resetear</button>
+        <button onClick = {restarContador}> - </button>
+        </>
+    )
+}
+
+
+export default ItemCount;
+
