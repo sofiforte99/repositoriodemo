d031a615b7c8fee44dfdb91ca90ffc9055391326
sofiforte99 <64369371+sofiforte99@users.noreply.github.com>
2021-01-29 15:37:13 -0300

    commit on 'master'

--- src/Cart.js
+++ src/Cart.js
@@ -1,12 +1,49 @@
 import { Button } from '@material-ui/core';
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react';
 import {CartContext} from './CartContext.js'
 import { Link } from 'react-router-dom';
 import CartItem from './CartItem.js';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import {firestore} from './firebaseConfig.js';
+import firebase from 'firebase/app';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 
 function Cart() {
     const { cart, clearProduct, total } = useContext(CartContext)
+    const [openPay, setOpenPay] = useState(false)
+    const [nombre, setNombre] = useState("")
+    const [telefono, setTelefono] = useState("")
+    const [email, setEmail] = useState("")
+
+    const classes = useStyles();
+
+    function enviarOrden(){
+        const db = firestore
+        const orders = db.collection ('orders')
+        
+        const order = {
+            cliente: {nombre:nombre, telefono:telefono, email: email},
+            items: cart,
+            date: firebase.firestore.Timestamp.fromDate(new Date()), 
+            total: total, 
+        }
+
+        orders.add(order)
+        .then(({id}) => alert("Escribir id de la compra" + id))
+        .catch ((error) => console.log (error))
+    }
+
+
 
     return (
         <div className="cart">
@@ -35,11 +72,27 @@ function Cart() {
                 <Button onClick={clearProduct} variant="contained" color="primary">
                     Vaciar carrito
                 </Button>
-                <Button onClick={() => {console.log(cart)}} variant="contained" color="primary">
-                    Finalizar compra
+                <Button onClick={() => {setOpenPay(true)}} variant="contained" color="primary">
+                 Finalizar Compra 
                 </Button>
+               
             </div>
             </>}
+
+
+            { openPay && 
+            <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="standard-basic" label="Nombre" value={nombre} 
+            onChange={(e) => setNombre(e.target.value)} />
+            <TextField id="standard-basic" label="Telefono" value={telefono} 
+            onChange={(e) => setTelefono(e.target.value)} />
+            <TextField id="standard-basic" label="Email" value={email} 
+            onChange={(e) => setEmail(e.target.value)} />
+            <Button onClick={enviarOrden} variant="contained" color="primary">
+                    Listo!
+                </Button>
+          </form>}
+
         </div>
     )
 }
